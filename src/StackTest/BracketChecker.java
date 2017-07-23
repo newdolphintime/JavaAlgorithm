96b399dfbad7fcc841ed300cd331c8fd615cad47
zhangwei <1551583950@qq.com>
2017-07-23 17:13:11 +0800

    栈的应用 语法检测器

---
+++ src/StackTest/BracketChecker.java
@@ -0,0 +1,53 @@
+package StackTest;
+
+/**
+ * Created by zhangwei on 2017/7/23.
+ * <p>
+ * 语法检查器
+ */
+public class BracketChecker {
+    private String input;
+
+    public BracketChecker(String input) {
+        this.input = input;
+
+    }
+
+    public void check() {
+        int stackSize = input.length();
+        StackX theStack = new StackX(stackSize);
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            switch (ch) {
+                case '{':
+                case '[':
+                case '(':
+                    theStack.push(ch);
+                    break;
+                case '}':
+                case ']':
+                case ')':
+                    if (!theStack.isEmpty()) {
+                        char chx = theStack.pop();
+                        if ((ch == '}' && chx != '{') ||
+                                (ch == ']' && chx != '[') ||
+                                (ch == ')' && chx != '(')) {
+                            System.out.println("Error :" + ch + " at" + i);
+                        } else {
+                            System.out.println(chx + " 成功匹配 " + ch);
+                        }
+
+                    } else {
+                        System.out.println("Error :" + ch + " at" + i);
+                    }
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BracketChecker bracketChecker = new BracketChecker("(1{2[3(4)5]6})");
+        bracketChecker.check();
+    }
+}
---
+++ src/StackTest/StackX.java
@@ -0,0 +1,37 @@
+package StackTest;
+
+/**
+ * Created by zhangwei on 2017/7/23.
+ * 栈的结构特点先进先出
+ */
+public class StackX {
+    private int maxSize;//栈的尺寸
+    private char[] stackArray;
+    private int top;
+
+    public StackX(int maxSize) {
+        this.maxSize = maxSize;
+        stackArray = new char[this.maxSize];
+        top = -1;
+    }
+
+    //推入操作
+    public void push(char c) {
+        stackArray[++top] = c;//top先加再操作
+    }
+
+    //推出一个元素
+    public char pop() {
+        return stackArray[top--];//top先推出再操作
+    }
+
+    //查看栈顶元素
+    public char peek() {
+        return stackArray[top];
+    }
+    //查看是不是空栈
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+}
