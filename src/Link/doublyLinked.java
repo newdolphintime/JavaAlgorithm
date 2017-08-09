2e78bbe1a1b3579d8f56d0f4bfd71d00c1e7ce96
newdolphintime <1551583950@qq.com>
2017-08-09 09:50:21 +0800

    双端链表

--- src/Link/doublyLinked.java
+++ src/Link/doublyLinked.java
@@ -75,11 +75,64 @@ public class doublyLinked {
         System.out.println("");
     }
 
+    public boolean insertAfter(int key ,int dd){
+        Link current = first;
+        while(current.dData != key){
+            current = current.next;
+            if(current == null){
+                return false;
+            }
+        }
+        Link newLink = new Link(dd);
+
+        if(current == last){
+            newLink.next = null;
+            last = newLink;
+        }
+        else{
+            newLink.next = current.next;
+            current.next.previous = newLink;
+        }
+        newLink.previous = current;
+        current.next = newLink;
+        return true;
+    }
+
+    public Link deleteKey(int key){
+        Link current = first;
+        while(current.dData!=key){
+            current = current.next;
+            if(current == null){
+                return null;
+            }
+        }
+
+        if(current==first){
+            first = current.next;
+        }else{
+            current.previous.next = current.next;
+        }
+
+        if(current==last){
+            last = current.previous;
+        }else{
+            current.next.previous = current.previous;
+        }
+        return current;
+    }
+
     public static void main(String[] args) {
         doublyLinked doublylinked = new doublyLinked();
-        doublylinked.insertFirst(5);
-        doublylinked.insertFirst(6);
         doublylinked.insertFirst(7);
+        doublylinked.insertFirst(6);
+        doublylinked.insertFirst(5);
+
+        doublylinked.insertLast(2);
+        doublylinked.insertLast(9);
+        doublylinked.insertLast(10);
+
+        doublylinked.deleteFirst();
+        doublylinked.deleteLast();
         doublylinked.displayForward();
         doublylinked.displayBackward();
 
