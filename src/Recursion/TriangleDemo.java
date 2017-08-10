bd02855e99217de32aef4be92bd3895dcf227427
zhangwei <1551583950@qq.com>
2017-08-10 22:19:21 +0800

    递归

--- src/Recursion/TriangleDemo.java
+++ src/Recursion/TriangleDemo.java
@@ -8,16 +8,30 @@ public class TriangleDemo {
 
     public static void main(String[] args) {
         theNumber = triangle(5);
-        System.out.println(theNumber);
+        System.out.println("三角数字"+theNumber);
+        theNumber = triangle(3);
+        System.out.println("阶乘"+theNumber);
     }
 
-    public static int triangle(int n){
-        if(n==1){
+    public static int triangle(int n) {
+        if (n == 1) {
             return 1;
 
-        }else{
+        } else {
             //递归
-            return (n+triangle(n-1));
+            //调用自身
+            //存在某个足够简单的问题层次，小道不需要调用自己也可以解答
+            return (n + triangle(n - 1));
+        }
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+
+        } else {
+            //阶乘
+            return (n * factorial(n - 1));
         }
     }
 
