03f059dcd387c9be066775c17905521d38385eb1
zhangwei <1551583950@qq.com>
2017-08-10 22:11:04 +0800

    递归

--- src/Recursion/TriangleDemo.java
+++ src/Recursion/TriangleDemo.java
@@ -4,6 +4,21 @@ package Recursion;
  * Created by zhangwei on 2017/8/8.
  */
 public class TriangleDemo {
+    static int theNumber;
 
+    public static void main(String[] args) {
+        theNumber = triangle(5);
+        System.out.println(theNumber);
+    }
+
+    public static int triangle(int n){
+        if(n==1){
+            return 1;
+
+        }else{
+            //递归
+            return (n+triangle(n-1));
+        }
+    }
 
 }
