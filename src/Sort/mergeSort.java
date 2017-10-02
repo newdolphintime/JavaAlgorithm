28adde4ec31e8f1c11a8f20c119d14fac3c5a2b0
zhangwei <1551583950@qq.com>
2017-10-02 22:32:36 +0800

    希尔排序

--- src/Sort/mergeSort.java
+++ src/Sort/mergeSort.java
@@ -7,6 +7,7 @@ package Sort;
 public class mergeSort {
     public static void main(String[] args) {
         int[] a = {7, 47, 81, 95};
+
         int[] b = {7, 14, 39, 55, 62, 74};
 
         int[] c = new int[10];
